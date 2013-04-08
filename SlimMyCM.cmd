ee6b6b278dd8b9e3a626fa852342180428936ac7
Daniele Lolli <uncledan@uncledan.it>
2013-04-08 20:12:46 +0200

    Release 4.13.1
    4.13.1 08/04/2013
    * graphic improvements
    * code clean-up
    * files clean-up
    * added "7z.dll" to make the script works without 7zip installed
    * "SlimMyCM.cmd" will not delete "Gallery2.apk" anymore (Gallery and
    Camera available out-of-the box)
    * added placeholders for "7z.exe" "7z.dll" and launcher and superuser
    APKs for easier github sync
    * "SlimMyGApps.cmd" minor fix (label NoGApps2)

--- SlimMyCM.cmd
+++ SlimMyCM.cmd
@@ -1,62 +1,66 @@
 @echo off
 cls
+type texts\logo.txt
+type texts\release.txt
 if not exist roms\*.zip goto NoRoms
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 echo Preparing workspace...
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 if exist roms-mod\*.zip del roms-mod\*.zip
 if exist roms-mod\*.zip-MOD del roms-mod\*.zip-MOD
 if exist roms-mod\*.tmp del roms-mod\*.tmp
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 echo Copying roms...
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 cd roms
 for %%f in (*.zip) do xcopy %%f ..\roms-mod\*.zip-MOD /V /F
 cd ..\roms-mod
 for %%f in (*.zip-MOD) do rename %%f *.MOD.zip
 if not exist *.zip goto NoRoms2
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 echo Cleaning up roms...
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 if exist cleanrom.txt for %%f in (*.zip) do ..\7z d -ssc %%f @cleanrom.txt
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 echo Cleaning up gapps...
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 if exist cleangapps.txt for %%f in (*.zip) do ..\7z d -ssc %%f @cleangapps.txt
 if not exist launcher\*.apk goto NoLauncher
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 echo Cleaning up launcher...
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 if exist cleanlauncher.txt for %%f in (*.zip) do ..\7z d -ssc %%f @cleanlauncher.txt
 :NoLauncher
 if not exist su\*.apk goto NoSU
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 echo Cleaning up su...
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 if exist cleansu.txt for %%f in (*.zip) do ..\7z d -ssc %%f @cleansu.txt
 :NoSU
 cd ..
-echo .
-echo .
+type texts\blank.txt
 echo All done.
 goto End
 :NoRoms
+type texts\blank.txt
 echo No ROMs in the rom folder.
 goto End
 :NoRoms2
+type texts\blank.txt
 echo No ROMs were copied to roms-mod folder.
 :End
+type texts\blank.txt
 echo Press a key to end...
 @pause>nul
--- SlimMyGApps.cmd
+++ SlimMyGApps.cmd
@@ -1,33 +1,40 @@
 @echo off
 cls
+type texts\logogapps.txt
+type texts\release.txt
 if not exist gapps\*.zip goto NoGApps
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 echo Preparing workspace...
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 if exist gapps-mod\*.zip del gapps-mod\*.zip
 if exist gapps-mod\*.zip-MOD del gapps-mod\*.zip-MOD
 if exist gapps-mod\*.tmp del gapps-mod\*.tmp
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 echo Copying gapps...
-echo .
-echo .
+type texts\blank.txt
+type texts\blank.txt
 cd gapps
 for %%f in (*.zip) do xcopy %%f ..\gapps-mod\*.zip-MOD /V /F
 cd ..\gapps-mod
 for %%f in (*.zip-MOD) do rename %%f *.MOD.zip
 if not exist *.zip goto NoGApps2
-echo .
-echo .
+type ..\texts\blank.txt
+type ..\texts\blank.txt
 echo Cleaning up gapps...
-echo .
-echo .
+type ..\texts\blank.txt
 if exist cleangapps.txt for %%f in (*.zip) do ..\7z d -ssc %%f @cleangapps.txt
 goto End
 :NoGApps
+type texts\blank.txt
 echo No GApps in the gapps folder.
+goto End
+:NoGApps2
+type texts\blank.txt
+echo No GApps were copied to roms-mod folder.
 :End
+type texts\blank.txt
 echo Press a key to end...
 @pause>nul
